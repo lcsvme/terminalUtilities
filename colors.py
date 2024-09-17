2bde173b69251c9524ae621422a5005da4862e63
Luan Vieira <luancarlosvieiras@gmail.com>
2024-09-17 15:04:50 -0300

    Create colors.py

---
+++ colors.py
@@ -0,0 +1,202 @@
+def info():
+    """
+    Creator: Luan Vieira
+    Date: 17/09/2024
+    Description: This file contains the colors and backgrounds for the terminal.
+    Example of usage: print(f'{red}Hello World{clear}') """
+
+    print(help(info))
+
+# Letter colors
+black = '\033[30m'
+red = '\033[31m'
+green = '\033[32m'
+yellow = '\033[33m'
+blue = '\033[34m'
+purple = '\033[35m'
+cyan = '\033[36m'
+white = '\033[37m'
+clear = '\033[m'
+
+# Bold letters colors
+bblack = '\033[30;1m'
+bred = '\033[31;1m'
+bgreen = '\033[32;1m'
+byellow = '\033[33;1m'
+bblue = '\033[34;1m'
+bpurple = '\033[35;1m'
+bcyan = '\033[36;1m'
+bwhite = '\033[37;1m'
+
+# Background colors
+backblack = '\033[40m'
+backred = '\033[41m'
+backgreen = '\033[42m'
+backyellow = '\033[43m'
+backblue = '\033[44m'
+backpurple = '\033[45m'
+backcyan = '\033[46m'
+backwhite = '\033[47m'
+
+# Background colors with black letters
+black_backblack = '\033[30;40m'
+black_backred = '\033[30;41m'
+black_backgreen = '\033[30;42m'
+black_backyellow = '\033[30;43m'
+black_backblue = '\033[30;44m'
+black_backpurple = '\033[30;45m'
+black_backcyan = '\033[30;46m'
+black_backwhite = '\033[30;47m'
+
+# Background colors with red letters
+red_backblack = '\033[31;40m'
+red_backred = '\033[31;41m'
+red_backgreen = '\033[31;42m'
+red_backyellow = '\033[31;43m'
+red_backblue = '\033[31;44m'
+red_backpurple = '\033[31;45m'
+red_backcyan = '\033[31;46m'
+red_backwhite = '\033[31;47m'
+
+# Background colors with green letters
+green_backblack = '\033[32;40m'
+green_backred = '\033[32;41m'
+green_backgreen = '\033[32;42m'
+green_backyellow = '\033[32;43m'
+green_backblue = '\033[32;44m'
+green_backpurple = '\033[32;45m'
+green_backcyan = '\033[32;46m'
+green_backwhite = '\033[32;47m'
+
+# Background colors with yellow letters
+yellow_backblack = '\033[33;40m'
+yellow_backred = '\033[33;41m'
+yellow_backgreen = '\033[33;42m'
+yellow_backyellow = '\033[33;43m'
+yellow_backblue = '\033[33;44m'
+yellow_backpurple = '\033[33;45m'
+yellow_backcyan = '\033[33;46m'
+yellow_backwhite = '\033[33;47m'
+
+# Background colors with blue letters
+blue_backblack = '\033[34;40m'
+blue_backred = '\033[34;41m'
+blue_backgreen = '\033[34;42m'
+blue_backyellow = '\033[34;43m'
+blue_backblue = '\033[34;44m'
+blue_backpurple = '\033[34;45m'
+blue_backcyan = '\033[34;46m'
+blue_backwhite = '\033[34;47m'
+
+# Background colors with purple letters
+purple_backblack = '\033[35;40m'
+purple_backred = '\033[35;41m'
+purple_backgreen = '\033[35;42m'
+purple_backyellow = '\033[35;43m'
+purple_backblue = '\033[35;44m'
+purple_backpurple = '\033[35;45m'
+purple_backcyan = '\033[35;46m'
+purple_backwhite = '\033[35;47m'
+
+# Background colors with cyan letters
+cyan_backblack = '\033[36;40m'
+cyan_backred = '\033[36;41m'
+cyan_backgreen = '\033[36;42m'
+cyan_backyellow = '\033[36;43m'
+cyan_backblue = '\033[36;44m'
+cyan_backpurple = '\033[36;45m'
+cyan_backcyan = '\033[36;46m'
+cyan_backwhite = '\033[36;47m'
+
+# Background colors with white letters
+white_backblack = '\033[37;40m'
+white_backred = '\033[37;41m'
+white_backgreen = '\033[37;42m'
+white_backyellow = '\033[37;43m'
+white_backblue = '\033[37;44m'
+white_backpurple = '\033[37;45m'
+white_backcyan = '\033[37;46m'
+white_backwhite = '\033[37;47m'
+
+# Background colors with bold black letters
+bblack_backblack = '\033[30;40;1m'
+bblack_backred = '\033[30;41;1m'
+bblack_backgreen = '\033[30;42;1m'
+bblack_backyellow = '\033[30;43;1m'
+bblack_backblue = '\033[30;44;1m'
+bblack_backpurple = '\033[30;45;1m'
+bblack_backcyan = '\033[30;46;1m'
+bblack_backwhite = '\033[30;47;1m'
+
+# Background colors with bold red letters
+bred_backblack = '\033[31;40;1m'
+bred_backred = '\033[31;41;1m'
+bred_backgreen = '\033[31;42;1m'
+bred_backyellow = '\033[31;43;1m'
+bred_backblue = '\033[31;44;1m'
+bred_backpurple = '\033[31;45;1m'
+bred_backcyan = '\033[31;46;1m'
+bred_backwhite = '\033[31;47;1m'
+
+# Background colors with bold green letters
+bgreen_backblack = '\033[32;40;1m'
+bgreen_backred = '\033[32;41;1m'
+bgreen_backgreen = '\033[32;42;1m'
+bgreen_backyellow = '\033[32;43;1m'
+bgreen_backblue = '\033[32;44;1m'
+bgreen_backpurple = '\033[32;45;1m'
+bgreen_backcyan = '\033[32;46;1m'
+bgreen_backwhite = '\033[32;47;1m'
+
+# Background colors with bold yellow letters
+byellow_backblack = '\033[33;40;1m'
+byellow_backred = '\033[33;41;1m'
+byellow_backgreen = '\033[33;42;1m'
+byellow_backyellow = '\033[33;43;1m'
+byellow_backblue = '\033[33;44;1m'
+byellow_backpurple = '\033[33;45;1m'
+byellow_backcyan = '\033[33;46;1m'
+byellow_backwhite = '\033[33;47;1m'
+
+# Background colors with bold blue letters
+bblue_backblack = '\033[34;40;1m'
+bblue_backred = '\033[34;41;1m'
+bblue_backgreen = '\033[34;42;1m'
+bblue_backyellow = '\033[34;43;1m'
+bblue_backblue = '\033[34;44;1m'
+bblue_backpurple = '\033[34;45;1m'
+bblue_backcyan = '\033[34;46;1m'
+bblue_backwhite = '\033[34;47;1m'
+
+# Background colors with bold purple letters
+bpurple_backblack = '\033[35;40;1m'
+bpurple_backred = '\033[35;41;1m'
+bpurple_backgreen = '\033[35;42;1m'
+bpurple_backyellow = '\033[35;43;1m'
+bpurple_backblue = '\033[35;44;1m'
+bpurple_backpurple = '\033[35;45;1m'
+bpurple_backcyan = '\033[35;46;1m'
+bpurple_backwhite = '\033[35;47;1m'
+
+# Background colors with bold cyan letters
+bcyan_backblack = '\033[36;40;1m'
+bcyan_backred = '\033[36;41;1m'
+bcyan_backgreen = '\033[36;42;1m'
+bcyan_backyellow = '\033[36;43;1m'
+bcyan_backblue = '\033[36;44;1m'
+bcyan_backpurple = '\033[36;45;1m'
+bcyan_backcyan = '\033[36;46;1m'
+bcyan_backwhite = '\033[36;47;1m'
+
+# Background colors with bold white letters
+bwhite_backblack = '\033[37;40;1m'
+bwhite_backred = '\033[37;41;1m'
+bwhite_backgreen = '\033[37;42;1m'
+bwhite_backyellow = '\033[37;43;1m'
+bwhite_backblue = '\033[37;44;1m'
+bwhite_backpurple = '\033[37;45;1m'
+bwhite_backcyan = '\033[37;46;1m'
+bwhite_backwhite = '\033[37;47;1m'
+
+if __name__ == '__main__':
+    info()
